7d5f156ec2265ba439f5723ab1a7a64a04aff723
Sergio Lo Porto <sergioloporto@gmail.com>
2019-11-08 12:00:34 +0100

    added load on scroll and counter

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@ $(function () {
     const key = "XZcghvRmHEXkbzbDf8QQxkocgmBGo5ndYeLKTIpR";
     const address = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos/";
     const $gallery = $(".gallery");
-    let isLoaded = false;
+    let isLoaded = true;
+    let counter = 0;
 
     function loadImages(sols) {
+        isLoaded = false;
         $.ajax({
             url: address,
             dataType: "json",
@@ -22,37 +24,32 @@ $(function () {
 
     loadImages(2050);
     loadImages(2051);
-    loadImages(2049);
-    loadImages(0);
 
     const insertImages = (images) => {
-        images.forEach( el => {
+        images.forEach( (el, i) => {
             const $newLi = $(`
                    <a data-fancybox="gallery" class="imagecontainer" href="${el.img_src}">
                    <img src="${el.img_src}" class="image">
                    </a>
             `);
-            $gallery.append($newLi)
-
-
+            // $gallery.append($newLi).fadeIn();
+            $gallery.append($newLi);
+            $newLi.hide().delay(i*400).fadeIn();
         });
     };
 
 
     $(window).scroll(function(el) {
         if($(window).scrollTop() + $(window).height() > $(document).height() - 500) {
-
             if (isLoaded === true) {
-
-            console.log("aaaaaaaaa")
-
-            // loadImages(0);
-
+                counter++;
+                console.log(counter);
+            loadImages(counter);
             }
 
 
         /* You can improve this code with following lines in ajax call:
-        new_element.hide().appendTo('.your_div').fadeIn(); $(window).scrollTop($(window).scrollTop()-1);
+        new_element.hide().delay(1).appendTo('.your_div').fadeIn(); $(window).scrollTop($(window).scrollTop()-1);
         First line appends elements in nice way, second assures that your function never stops at the bottom of the page. */
 
         }
